27d4dedbc5ad900640f795f9cf3dfd97d53db18f
amlewis1578 <amlewis1578@gmail.com>
2024-05-07 12:18:34 -0600

    adding to readme and example file, and updating version

--- docs/versions.md
+++ docs/versions.md
@@ -1,3 +1,4 @@
 **0**
 - `0.1.0` - `Particle` and `ElasticChannel` classes
 - `0.2.0` - `CaptureChannel` classes
+- `0.3.0` - `SpinGroup` classes
--- src/rmatrix/__init__.py
+++ src/rmatrix/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from rmatrix.base.particles import Particle
 from rmatrix.channels.elastic_channel import ElasticChannel
